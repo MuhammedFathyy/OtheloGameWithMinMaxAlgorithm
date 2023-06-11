8339559b0f22af7e79ed63f0e62d67044beec0b2
farahmrwan16333 <farahmrwan33@gmail.com>
2023-06-11 20:53:20 +0300

    board

--- src/main/java/org/Othello/Board/Board.java
+++ src/main/java/org/Othello/Board/Board.java
@@ -1,6 +1,6 @@
 package org.Othello.Board;
 
-public class Board {
+public class Board  {
 
     private static final int SIZE = 8;
 
@@ -11,6 +11,22 @@ public class Board {
     }
 
     public Board() {
+        for(int )
+    }
+     public Board(Coordinates position, int width, int height, int gridWidth, int gridHeight) {
+        super(position, width, height);
+        grid = new GridCell[gridWidth][gridHeight];
+        int cellWidth = (width-position.x)/gridWidth;
+        int cellHeight = (height-position.y)/gridHeight;
+        for(int x = 0; x < gridWidth; x++) {
+            for(int y = 0; y < gridHeight; y++) {
+                grid[x][y] = new GridCell(new Position(position.x+cellWidth*x, position.y+cellHeight*y),
+                        cellWidth, cellHeight);
+            }
+        }
+        moveNumber = 0;
+        validMoves = new ArrayList<>();
+        updateValidMoves(1);
     }
 
     public int getRow()
