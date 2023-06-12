e7a7902049a8fb0057a209645235d2f431420f52
MuhammadMoataz <72518785+MuhammadMoataz@users.noreply.github.com>
2023-06-12 21:55:08 +0300

    Determine Depth and time using difficulty

--- src/main/java/org/Othello/MiniMax/IterativeDeepening.java
+++ src/main/java/org/Othello/MiniMax/IterativeDeepening.java
@@ -4,9 +4,13 @@ import org.Othello.Board.Board;
 import org.Othello.Board.Coordinates;
 
 public class IterativeDeepening {
+    private int MAX_DEPTH = 2;
+    private long TIME_LIMIT = 2000;
     private static final MinimaxWithAlphaBeta minimaxAlphaBeta = new MinimaxWithAlphaBeta();
 
-    public Coordinates findBestMove(Board board, int MAX_DEPTH, long TIME_LIMIT, String diff) {
+    public Coordinates findBestMove(Board board, String diff) {
+        MAX_DEPTH =  (diff.equalsIgnoreCase("HARD"))? MAX_DEPTH * 3 : MAX_DEPTH;
+        TIME_LIMIT =  (diff.equalsIgnoreCase("HARD"))? TIME_LIMIT * 3 : TIME_LIMIT;
         long startTime = System.currentTimeMillis();
         Coordinates bestMove = null;
 
